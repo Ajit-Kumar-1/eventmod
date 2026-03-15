4c682bc2d541af939c021f3436b00a2a4c82313e
Ajit Kumar <ajitkumarxc8@gmail.com>
2026-03-15 15:06:35 +0000

    Refactor

--- backend/api/Utils.ts
+++ backend/api/Utils.ts
@@ -1,7 +1,10 @@
 import type { Event } from "../data/Types.ts";
 
-export const claimedBySomeoneElse = (event: Event, user_id: string): boolean => !!event
-  && event.claimedBy !== user_id
-  && !!event.claimedAt
+const claimValid = (event: Event): boolean => !!event?.claimedAt
   && new Date().getTime() - new Date(event.claimedAt).getTime() < 15 * 60 * 1000;
-;
+
+export const claimedBySomeoneElse = (event: Event, user_id: string): boolean => claimValid(event)
+  && event.claimedBy !== user_id;
+
+export const claimedByMe = (event: Event, user_id: string): boolean => claimValid(event)
+  && event.claimedBy === user_id;
