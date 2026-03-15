5b672d33177490783f236d56465bff27044ad7b8
Ajit Kumar <ajitkumarxc8@gmail.com>
2026-03-15 17:45:00 +0000

    Refactoring param name

--- backend/api/Utils.ts
+++ backend/api/Utils.ts
@@ -3,5 +3,5 @@ import type { Event } from "../Types.ts";
 export const claimValid = (event: Event): boolean => !!event?.claimedAt
   && new Date().getTime() - new Date(event.claimedAt).getTime() < 15 * 60 * 1000;
 
-export const claimedByMe = (event: Event, user_id: string): boolean => claimValid(event)
-  && event.claimedBy === user_id;
+export const claimedByMe = (event: Event, userId: string): boolean => claimValid(event)
+  && event.claimedBy === userId;
