76e10cc573adbf4e81dc1aec06250e7f959bbf0d
Ajit Kumar <ajitkumarxc8@gmail.com>
2026-03-15 15:14:55 +0000

    Adding acknowledgement route

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,9 +1,11 @@
 import loginRoute from './login/index.ts';
 import eventsRoute from './events/index.ts';
 import claimRoute from './claim/index.ts';
+import acknowledgeRoute from './acknowledge/index.ts';
 
 export {
   loginRoute,
   eventsRoute,
   claimRoute,
+  acknowledgeRoute,
 };
--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { loginRoute, eventsRoute, claimRoute } from './api/index.ts';
+import { loginRoute, eventsRoute, claimRoute, acknowledgeRoute } from './api/index.ts';
 const app = express()
 const port = 3000
 
@@ -8,6 +8,7 @@ app.use(express.json())
 app.post(loginRoute.path, loginRoute.handler)
 app.get(eventsRoute.path, eventsRoute.handler)
 app.put(claimRoute.path, claimRoute.handler)
+app.put(acknowledgeRoute.path, acknowledgeRoute.handler)
 
 
 app.listen(port, () => {
