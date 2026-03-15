9ebcf0d1b90edd22c0cd27fa7b6e60d11decf943
Ajit Kumar <ajitkumarxc8@gmail.com>
2026-03-15 17:43:34 +0000

    Updated cron

--- backend/api/ResetExpiredHandler.ts
+++ backend/api/ResetExpiredHandler.ts
@@ -4,16 +4,19 @@ import { claimValid } from './Utils.ts';
 
 export default function resetExpired() {
   fs.readFile('./data/events.json', 'utf8', (err, eventData) => {
-    if (err) return;
+    if (err) return console.log('Failed to read event data');
 
     let events: Event[] = JSON.parse(eventData);
-    events = events.filter((e: Event) => !(e.status === Status.CLAIMED && !claimValid(e)));
+    events = events.map((e: Event) => {
+      if (e.status === Status.CLAIMED && !claimValid(e)) {
+        return { ...e, status: Status.OPEN, claimedBy: null, claimedAt: null };
+      }
+      return e;
+    });
 
     fs.writeFile('./data/events.json', JSON.stringify(events, null, 2), (err) => {
-      if (err) {
-        return console.error('Failed to update event data');
-      }
-      return console.log('Event claimed successfully');
+      if (err) return console.warn('Failed to update event data');
+      return console.log('Event statuses updated');
     });
   });
 }
